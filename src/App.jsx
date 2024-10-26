df9645033bf296485c02802a80644add1bbc18d1
RieNah <rinahratsimilo@gmail.com>
2024-10-26 05:59:40 +0200

    Router

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/home/home';
+import './App.css';
+import About from './pages/aboutPage/about';
+function App() {
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutPage" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,13 @@
+import logo from "../assets/logo.png"
+import { Link } from "react-router-dom"
 function Header(){
     return(
         <header>
-
+            <img src={logo} alt="logo" />
+            <nav>
+                <Link to="/">Acceuil</Link>
+                <Link to="/aboutPage">A propos</Link>
+            </nav>
         </header>
     )
 }
